6bf62a59ff4d214fabe0b95806686726feb25f14
seyedmohammadreza <59357443+mrakharatha@users.noreply.github.com>
2023-10-27 14:53:57 +0330

    CollectionControls

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -7,29 +7,31 @@ public partial class App : Application
 		InitializeComponent();
 
 
-        var navigationPage = new NavigationPage(new MainPage())
-        {
-            //BarBackground = Colors.Chocolate,
-            //BarTextColor = Colors.White
-        };
+		var navigationPage = new NavigationPage(new MainPage())
+		{
+			//BarBackground = Colors.Chocolate,
+			//BarTextColor = Colors.White
+		};
 
 
-        //MainPage = navigationPage;
+		//MainPage = navigationPage;
 
-        //MainPage = new FlyoutPageDemo();
+		//MainPage = new FlyoutPageDemo();
 
-        //MainPage = new TabbedPageDemo();
+		//MainPage = new TabbedPageDemo();
 
-        // MainPage = new StackLayoutDemo();
+		// MainPage = new StackLayoutDemo();
 
-        //MainPage = new GridDemo();
+		//MainPage = new GridDemo();
 
-       // MainPage = new AbsoluteDemo();
-        
-     //  MainPage = new FlexDemo();
-       
-    // MainPage = new CommandControls();
-    
-    MainPage = new InputControls();
-    }
+		// MainPage = new AbsoluteDemo();
+
+		//  MainPage = new FlexDemo();
+
+		// MainPage = new CommandControls();
+
+		//MainPage = new InputControls();
+
+		MainPage = new CollectionControls();
+	}
 }
---
+++ MauiApp1/CollectionControls.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class CollectionControls : ContentPage
+{
+	public CollectionControls()
+	{
+		InitializeComponent();
+	}
+}
